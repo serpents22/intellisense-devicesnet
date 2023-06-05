5312796c91896c1d8b5f581834a89415add06b2f
serpents22 <ivandarmawan67@gmail.com>
2023-06-05 18:11:51 +0700

    fix: change realtime flow

--- src/views/Map/RealtimeMap.vue
+++ src/views/Map/RealtimeMap.vue
@@ -1,120 +1,110 @@
 <template>
-  <div id="mapRoot"></div>
+  <div ref="mapContainer" class="map-container"></div>
+  <div class="absolute left-[80px] top-[82px] z-30 bg-white rounded-lg w-fit h-fit h-max-[1000px] shadow-2xl px-4 py-4 flex flex-col gap-2">
+    <h1 class="text-left font-bold text-lg">Select Vehicle</h1>
+    <select v-model="selectedVehicle" class="cursor-pointer" @change="changeVehicle(selectedVehicle)">
+      <option v-for="item in devicesList" :key="item.id" :value="item.IMEINumber">{{ item.deviceName }}</option>
+    </select>
+  </div>
 </template>
 
 <script setup>
-import 'ol/ol.css';
-import * as ol from 'ol';
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
-import { getCenter } from 'ol/extent.js';
-import { defaults as defaultInteractions } from 'ol/interaction.js';
+import { ref, onMounted } from 'vue'
+import { Map, View } from 'ol'
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
+import { OSM, Vector as VectorSource } from 'ol/source'
+import { fromLonLat } from 'ol/proj'
+import Feature from 'ol/Feature'
+import Point from 'ol/geom/Point'
+import { Style, Circle, Fill, Stroke } from 'ol/style'
+import { useGeoDataStore } from '@/stores/GeoDataStore'
+import { useDevicesStore } from '@/stores/DevicesStore'
 
-import { Icon, Style } from 'ol/style.js';
-import { Point } from 'ol/geom.js';
-import { Vector as VectorLayer } from 'ol/layer.js';
-import { Vector } from 'ol/source.js';
+import { storeToRefs } from 'pinia'
 
-import ImageLayer from 'ol/layer/Image.js'
-import Projection from 'ol/proj/Projection.js';
-import Static from 'ol/source/ImageStatic.js';
+let map
+let vectorSource
+let marker
+const whileState = ref(true)
+const mapContainer = ref(null)
+const geoDataStore = useGeoDataStore()
+const devicesStore = useDevicesStore()
+const { deviceGeo } = storeToRefs(useGeoDataStore())
+const { devicesList } = storeToRefs(useDevicesStore())
+const selectedVehicle = ref(null)
+const delay = require('delay')
 
-import { ref, onMounted, onUnmounted } from 'vue';
 
+const queryParams = ref({
+  imei: selectedVehicle.value
+})
 
+onMounted( async () => {
+  await devicesStore.loadDevices()
+  selectedVehicle.value = devicesList.value[0].IMEINumber
+  initializeMap()
+  queryParams.value.imei = devicesList.value[0].IMEINumber
+  getCoordinates()
+  while (whileState) {
+    getCoordinates()
+    await delay(3000)
+  }
+})
 
-const extent = [-500, 0, 500, 500];
-const projection = new Projection({
-  code: 'my-custom-projection',
-  units: 'pixels',
-  extent: extent,
-});
-
-onMounted(() => {
-  const map = new Map({
-    layers: [],
-    target: 'mapRoot',
+function initializeMap() {
+  map = new Map({
+    target: mapContainer.value,
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
     view: new View({
-      projection: projection,
-      center: getCenter(extent),
-      zoom: 2.6,
-      minZoom: 2.6,
-      maxZoom: 6,
+      center: fromLonLat([0, 0]),
+      zoom: 14,
     }),
-    interactions: defaultInteractions({
-      dragAndDrop: false,
-      dragPan: false,	
-      keyboardPan: false,      
-    }),
-  });
-
-  //create image layer
-  const imgLayer = new ImageLayer({
-    source: new Static({
-      attributions: '© <a href="https://google.com">SCBD HABITAT ZOO</a>',
-      url: require('../../assets/img/map-overlay.jpg'),
-      projection: projection,
-      imageExtent: extent,
-    }),
-  })
-  map.addLayer(imgLayer); // add the layer to the map
-
-  //create marker layer
-  const markerLayer = new VectorLayer({
-    source: new Vector({
-      features: [
-        new ol.Feature({
-          geometry: new Point([0, 0]), // adjust the coordinates to match your marker
-        }),
-        new ol.Feature({
-          geometry: new Point([0.5, 0]), // adjust the coordinates to match your marker
-        }),
-        new ol.Feature({
-          geometry: new Point([1, 0]), // adjust the coordinates to match your marker
-        }),
-      ],
-    }),
-    style: new Style({
-      image: new Icon({
-        src: require('../../assets/img/green-dot.png'), // adjust the path to match the location of your marker image
-        anchor: [0.5, 1],
-      }),
-    }),
-  });
-  
-  map.addLayer(markerLayer); // add the layer to the map
-  
-  const markerLayer2 = new VectorLayer({
-    source: new Vector({
-      features: [],
-    }),
-    style: new Style({
-      image: new Icon({
-        src: require('../../assets/img/marker-orange.png'), // adjust the path to match the location of your marker image
-        anchor: [0.5, 1],
-      }),
-    }),
-  });
-  
-  map.addLayer(markerLayer2); // add the layer to the map
-
-
-
-  const feature = new ol.Feature({
-    geometry: new Point([100, 0]), // adjust the coordinates to match your marker
+    controls:[]
   })
 
-  markerLayer2.getSource().addFeature(feature)
+  vectorSource = new VectorSource();
+  map.addLayer(new VectorLayer({ source: vectorSource }));
 
+  marker = new Feature({
+    geometry: new Point(fromLonLat([0, 0])),
+  });
+  marker.setStyle(
+    new Style({
+      image: new Circle({
+        radius: 6,
+        fill: new Fill({ color: 'red' }),
+        stroke: new Stroke({ color: 'white', width: 2 }),
+      }),
+    })
+  );
+  vectorSource.addFeature(marker)
 
-});
+}
 
+function addMarkersToMap(coordinate) {
+  console.log(coordinate)
+  marker.getGeometry().setCoordinates(coordinate);
+  map.getView().setCenter(coordinate);  
+}
 
+async function getCoordinates() {
+  await geoDataStore.getLastDeviceGeo(queryParams.value)
+  addMarkersToMap(fromLonLat([deviceGeo.value.longitude, deviceGeo.value.latitude]))
+}
+
+function changeVehicle(selectedVehicle) {
+  queryParams.value.imei = selectedVehicle
+}
 
 </script>
 
 <style scoped>
-#mapRoot {
-  @apply w-full h-screen;
+.map-container {
+  width: 100%;
+  height: 100vh
 }
 </style>
